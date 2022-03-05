84c7477736a5ba8665fc937eb80bed2a2df1665a
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-05 13:32:21 +0700

    FindById

--- repository/category_repository_mock.go
+++ repository/category_repository_mock.go
@@ -1,7 +1,21 @@
 package repository
 
-import "github.com/stretchr/testify/mock"
+import (
+	"go-unit-test/entity"
+
+	"github.com/stretchr/testify/mock"
+)
 
 type CategoryRepositoryMock struct {
 	Mock mock.Mock
 }                                                     
+
+func (repo *CategoryRepositoryMock) FindById (id string) *entity.Category {
+	args := repo.Mock.Called(id)
+	if args.Get(0) == nil {
+		return nil
+	} else {
+		category := args.Get(0).(entity.Category)
+		return &category
+	}
+}
